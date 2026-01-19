23cf263255ff67deed03984e29c9643810cbc050
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2026-01-19 23:39:08 +0530

    Time: 0 ms (100%), Space: 16.6 MB (87.45%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int fresh = 0;
+
+       
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2)
+                    q.push({i, j});
+                else if(grid[i][j] == 1)
+                    fresh++;
+            }
+        }
+
+      
+        if(fresh == 0) return 0;
+
+        int time = 0;
+        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+       
+        while(!q.empty()) {
+            int size = q.size();
+            bool rotted = false;
+
+            while(size--) {
+                auto [r, c] = q.front();
+                q.pop();
+
+                for(auto &dir : directions) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;  
+                        fresh--;
+                        q.push({nr, nc});
+                        rotted = true;
+                    }
+                }
+            }
+
+            if(rotted) time++; 
+        }
+
+        
+        return (fresh == 0) ? time : -1;
+    }
+};
