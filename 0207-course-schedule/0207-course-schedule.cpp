29c0c46e3866ca702cecd98067a0a2384a49bbd0
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2026-01-21 19:27:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.68%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses,0);
+        queue<int>q;
+        vector<int>ans;
+        for(int i=0;i<prerequisites.size();i++){
+            int course = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            adj[pre].push_back(course);
+            indegree[course]++;
+        }
+        
+    
+        for(int i=0;i<numCourses;i++){
+            if(!indegree[i]){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(int j=0;j<adj[node].size();j++){
+                {
+                    indegree[adj[node][j]]--;
+                    if(indegree[adj[node][j]]==0){
+                        q.push(adj[node][j]);
+                    }
+                }
+            }
+        }
+    int count =ans.size();
+    return count==numCourses;
+        
+    }
+    
+};
