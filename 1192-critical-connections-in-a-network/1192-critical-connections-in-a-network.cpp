21df84f09820327a03cb3d2d6bb4286bbaa04309
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2026-02-02 13:02:39 +0530

    Time: 219 ms (30.7%), Space: 185.2 MB (19.31%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void DFS(int node,int par,vector<vector<int>>&adj,vector<int>&disc,vector<int>&low,vector<bool>&visited,vector<vector<int>>&bridges,int &count){
+        disc[node]=low[node]=count;
+        visited[node]=1;
+        for(int j=0;j<adj[node].size();j++){
+            int neig=adj[node][j];
+            if(neig==par)
+            continue;
+            else if(visited[neig]){
+                low[node]=min(low[node],low[neig]);
+            }
+            else{
+                count++;
+                DFS(neig,node,adj,disc,low,visited,bridges,count);
+                if(low[neig]>disc[node]){
+                    vector<int>temp;
+                    temp.push_back(neig);
+                    temp.push_back(node);
+                    bridges.push_back(temp);
+                }
+                low[node]=min(low[node],low[neig]);
+            }
+        }
+
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>>adj(n);
+        for(int i=0;i<connections.size();i++){
+            
+                int u=connections[i][0];
+                int v=connections[i][1];
+                adj[u].push_back(v);
+                adj[v].push_back(u);
+
+        }
+        vector<vector<int>>bridges;
+        vector<int>disc(n);
+        vector<int>low(n);
+        vector<bool>visited(n,0);
+        int count=0;
+        DFS(0,-1,adj,disc,low,visited,bridges,count);
+        return bridges;
+
+
+        
+    }
+};
